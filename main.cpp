61f1bb8a347d621da1e0fbd5f7986a66dc72c052
Ahmed Elawam <arelawam@gmail.com>
2023-02-16 18:17:31 +0200

    adding average function

--- main.cpp
+++ main.cpp
@@ -9,11 +9,17 @@ int getSum(std::vector<int> &input_vector){
     return sum;
 }
 
+float getAverage(std::vector<int> &input_vector){
+    int sum = getSum(input_vector);
+    return sum / input_vector.size();
+}
+
 int main(){
 
     std::vector<int> data = {9,4,3,16,5,-11,6,88,801,-900};
 
     std::cout << getSum(data) << "\n";
+    std:: cout << getAverage(data) << "\n";
 
     return 0;
 }
